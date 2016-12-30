1a564b77a5149df3543975787b718be2b036bf15
LightAir <e.konkin@cb-cp.ru>
2016-12-30 12:17:56 +0300

    codecov 3

--- tests-run.sh
+++ tests-run.sh
@@ -4,7 +4,7 @@ php -S localhost:3351 -t testing/srv > /dev/null 2>&1 &
 php_pid=$!
 export CURL_TEST_SERVER_RUNNING=1
 
-vendor/bin/phpunit $@
+vendor/bin/phpunit $@  --coverage-clover=coverage.xml
 ret=$?
 
 if [ $CURL_TEST_SERVER_RUNNING ]; then
